bbb3faeb3cdf1048131d1e5aff2de5873a9cd068
TC <teresa.j.campbell@gmail.com>
2022-02-13 12:49:45 -0500

    created connection to mysql

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended:false}));
 app.use(express.json());
 
+//Connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password 
+        password :'ChloeAmelia1979!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 
